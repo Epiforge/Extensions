9f7914aa3f76ee04057f3444c67541b8f8fd5c7c
Daniel Henry <daniel.henry@epiforge.com>
2023-08-14 23:49:04 -0400

    Platforms.Windows: added SourceLink and improved P/Invoke performance

--- Epiforge.Extensions.Platforms.Windows/ConsoleAssist.cs
+++ Epiforge.Extensions.Platforms.Windows/ConsoleAssist.cs
@@ -17,12 +17,20 @@ public static class ConsoleAssist
         if (process is null)
             throw new ArgumentNullException(nameof(process));
 #endif
+#if IS_NET_7_0_OR_GREATER
+        return NativeMethods.AttachConsole((uint)process.Id);
+#else
         return NativeMethods.AttachConsole(process.Id);
+#endif
     }
 
     /// <summary>
     /// Attaches to the console of the parent process as a client application
     /// </summary>
     public static bool AttachToParentProcess() =>
+#if IS_NET_7_0_OR_GREATER
+        NativeMethods.AttachConsole(unchecked((uint)-1));
+#else
         NativeMethods.AttachConsole(-1);
+#endif
 }
--- Epiforge.Extensions.Platforms.Windows/NativeInterop/NativeMethods.cs
+++ Epiforge.Extensions.Platforms.Windows/NativeInterop/NativeMethods.cs
@@ -1,12 +1,61 @@
 namespace Epiforge.Extensions.Platforms.Windows.NativeInterop;
 
-static class NativeMethods
+static partial class NativeMethods
 {
+#if IS_NET_7_0_OR_GREATER
+    [LibraryImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool AttachConsole(uint dwProcessId);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool GetCursorPos(out Types.Point lpPoint);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool FlashWindow(IntPtr hwnd, [MarshalAs(UnmanagedType.Bool)] bool bInvert);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool FlashWindowEx(ref FlashWInfo pwfi);
+
+    [LibraryImport("user32.dll")]
+    public static partial IntPtr GetDesktopWindow();
+
+    [LibraryImport("user32.dll")]
+    public static partial IntPtr GetForegroundWindow();
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool GetLastInputInfo(ref LastInputInfo lastInputInfo);
+
+    [LibraryImport("user32.dll")]
+    public static partial IntPtr GetShellWindow();
+
+    [DllImport("kernel32.dll")]
+    public static extern uint GetTickCount();
+
+    [LibraryImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool GetWindowRect(IntPtr hWnd, out Rect rect);
+
+    [LibraryImport("ntdll.dll")]
+    public static partial int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref ProcessBasicInformation processInformation, int processInformationLength, out int returnLength);
+
+    [LibraryImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool SetCursorPos(int x, int y);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool SetForegroundWindow(IntPtr hWnd);
+#else
     [DllImport("kernel32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool AttachConsole([MarshalAs(UnmanagedType.U4)] int dwProcessId);
 
     [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetCursorPos(out Types.Point lpPoint);
 
     [DllImport("user32.dll")]
@@ -24,6 +73,7 @@ static class NativeMethods
     public static extern IntPtr GetForegroundWindow();
 
     [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetLastInputInfo(ref LastInputInfo lastInputInfo);
 
     [DllImport("user32.dll")]
@@ -33,15 +83,18 @@ static class NativeMethods
     public static extern uint GetTickCount();
 
     [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetWindowRect(IntPtr hWnd, out Rect rect);
 
     [DllImport("ntdll.dll")]
     public static extern int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref ProcessBasicInformation processInformation, int processInformationLength, out int returnLength);
 
     [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool SetCursorPos(int x, int y);
 
     [DllImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool SetForegroundWindow(IntPtr hWnd);
+#endif
 }
