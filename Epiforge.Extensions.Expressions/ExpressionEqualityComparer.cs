42ba762f1a1e7cb9c77267e873c47d6400de5183
Daniel Henry <daniel.henry@gmail.com>
2026-04-12 15:43:26 -0400

    improved the performance of ExpressionEqualityComparer with a weak cache; Expressions revision

--- Epiforge.Extensions.Expressions/ExpressionEqualityComparer.cs
+++ Epiforge.Extensions.Expressions/ExpressionEqualityComparer.cs
@@ -6,6 +6,9 @@ namespace Epiforge.Extensions.Expressions;
 public sealed class ExpressionEqualityComparer :
     IEqualityComparer<Expression>
 {
+    static readonly ConditionalWeakTable<Expression, IReadOnlyList<object?>> cachedDiagrams = [];
+    static readonly ConditionalWeakTable<Expression, object> cachedHashCodes = [];
+
     /// <summary>
     /// Gets the default instance of <see cref="ExpressionEqualityComparer"/>
     /// </summary>
@@ -21,8 +24,16 @@ public sealed class ExpressionEqualityComparer :
     {
         if (ReferenceEquals(x, y))
             return true;
-        using var xDiagramEnumerator = ExpressionDiagramGenerator.GenerateDiagram(x).GetEnumerator();
-        using var yDiagramEnumerator = ExpressionDiagramGenerator.GenerateDiagram(y).GetEnumerator();
+        if (x is null || y is null)
+            return false;
+        cachedDiagrams.TryGetValue(x, out var cachedXDiagram);
+        cachedDiagrams.TryGetValue(y, out var cachedYDiagram);
+        if (cachedXDiagram is not null && cachedYDiagram is not null)
+            return cachedXDiagram.SequenceEqual(cachedYDiagram);
+        var xDiagram = cachedXDiagram is not null ? null : new List<object?>();
+        var yDiagram = cachedYDiagram is not null ? null : new List<object?>();
+        using var xDiagramEnumerator = cachedXDiagram is null ? ExpressionDiagramGenerator.GenerateDiagram(x).GetEnumerator() : cachedXDiagram.GetEnumerator();
+        using var yDiagramEnumerator = cachedYDiagram is null ? ExpressionDiagramGenerator.GenerateDiagram(y).GetEnumerator() : cachedYDiagram.GetEnumerator();
         while (true)
         {
             var xDiagramEnumeratorMoved = xDiagramEnumerator.MoveNext();
@@ -30,9 +41,17 @@ public sealed class ExpressionEqualityComparer :
             if (xDiagramEnumeratorMoved != yDiagramEnumeratorMoved)
                 return false;
             if (!xDiagramEnumeratorMoved)
+            {
+                if (xDiagram is not null)
+                    cachedDiagrams.AddOrUpdate(x, xDiagram.AsReadOnly());
+                if (yDiagram is not null)
+                    cachedDiagrams.AddOrUpdate(y, yDiagram.AsReadOnly());
                 return true;
+            }
             if (!Equals(xDiagramEnumerator.Current, yDiagramEnumerator.Current))
                 return false;
+            xDiagram?.Add(xDiagramEnumerator.Current);
+            yDiagram?.Add(yDiagramEnumerator.Current);
         }
     }
 
@@ -43,10 +62,22 @@ public sealed class ExpressionEqualityComparer :
     /// <returns>A hash code for the specified expression tree</returns>
     public int GetHashCode(Expression? obj)
     {
+        if (obj is not null && cachedHashCodes.TryGetValue(obj, out var cachedHashCode))
+            return (int)cachedHashCode;
         var hashCode = new System.HashCode();
-        if (obj is not null)
-            foreach (var element in ExpressionDiagramGenerator.GenerateDiagram(obj))
-                hashCode.Add(element);
-        return hashCode.ToHashCode();
+        if (obj is null)
+            return hashCode.ToHashCode();
+        cachedDiagrams.TryGetValue(obj, out var cachedObjDiagram);
+        var objDiagram = cachedObjDiagram is not null ? null : new List<object?>();
+        foreach (var element in cachedObjDiagram ?? ExpressionDiagramGenerator.GenerateDiagram(obj))
+        {
+            hashCode.Add(element);
+            objDiagram?.Add(element);
+        }
+        if (objDiagram is not null)
+            cachedDiagrams.AddOrUpdate(obj, objDiagram.AsReadOnly());
+        var result = hashCode.ToHashCode();
+        cachedHashCodes.AddOrUpdate(obj, result);
+        return result;
     }
 }
